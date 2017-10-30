c131b689aae2568ca4a33c233b37d939d1f74b2c
Jobayer Arman <jobayer.arman@gmail.com>
2017-10-30 22:36:59 +0600

    added typography basic style

--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -10,27 +10,27 @@
                 <div class="container">
                     <div class="section-inner">
                         <header class="row">
-                            <div class="col-xs-12">
+                            <div class="col-12">
                                 <h3 class="text-center">Our Services</h3>
                                 <hr class="section-header">
                             </div>
                         </header>
-                        <div class="row">
-                            <div class="col-md-4 col-xs-12">
+                        <div class="row text-center">
+                            <div class="col-md-4 col-12">
                                 <div class="service-feature">
                                     <h5>Photography</h5>
                                     <i class="fa fa-3x fa-camera" aria-hidden="true"></i>
                                     <p></p>
                                 </div>
                             </div>
-                            <div class="col-md-4 col-xs-12">
+                            <div class="col-md-4 col-12">
                                 <div class="service-feature">
                                     <h5>Cinematography/Videography</h5>
                                     <i class="fa fa-3x fa-video-camera" aria-hidden="true"></i>
                                     <p></p>
                                 </div>
                             </div>
-                            <div class="col-md-4 col-xs-12">
+                            <div class="col-md-4 col-12">
                                 <div class="service-feature">
                                     <h5>Photobook</h5>
                                     <i class="fa fa-3x fa-book" aria-hidden="true"></i>
@@ -46,21 +46,21 @@
                 <div class="container">
                     <div class="section-inner">
                         <header class="row">
-                            <div class="col-xs-12">
+                            <div class="col-12">
                                 <h3 class="text-center">Our Packages</h3>
                                 <hr class="section-header">
                             </div>
                         </header>
                         <div class="row">
-                            <div class="col-md-4 col-xs-12">
+                            <div class="col-md-4 col-12">
                                 <h5 class="text-center">Economy</h5>
                                 <p></p>
                             </div>
-                            <div class="col-md-4 col-xs-12">
+                            <div class="col-md-4 col-12">
                                 <h5 class="text-center">Sliver</h5>
                                 <p></p>
                             </div>
-                            <div class="col-md-4 col-xs-12">
+                            <div class="col-md-4 col-12">
                                 <h5 class="text-center">Gold</h5>
                                 <p></p>
                             </div>
