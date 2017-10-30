81210f05ed29cc3fb632f136e14249cdc5d8896f
Jobayer Arman <jobayer.arman@gmail.com>
2017-10-31 00:20:41 +0600

    update frontpage

--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -5,7 +5,6 @@
  * @package canvas
  */
 ?>
-
             <section class="homepage-section">
                 <div class="container">
                     <div class="section-inner">
@@ -18,22 +17,22 @@
                         <div class="row text-center">
                             <div class="col-md-4 col-12">
                                 <div class="service-feature">
-                                    <h5>Photography</h5>
                                     <i class="fa fa-3x fa-camera" aria-hidden="true"></i>
+                                    <h5>Photography</h5>
                                     <p></p>
                                 </div>
                             </div>
                             <div class="col-md-4 col-12">
                                 <div class="service-feature">
-                                    <h5>Cinematography/Videography</h5>
                                     <i class="fa fa-3x fa-video-camera" aria-hidden="true"></i>
+                                    <h5>Cinematography/Videography</h5>
                                     <p></p>
                                 </div>
                             </div>
                             <div class="col-md-4 col-12">
                                 <div class="service-feature">
-                                    <h5>Photobook</h5>
                                     <i class="fa fa-3x fa-book" aria-hidden="true"></i>
+                                    <h5>Photobook</h5>
                                     <p></p>
                                 </div>
                             </div>
